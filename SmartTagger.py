fa78e6349f6afcf34abf4f501e8f10264bef307f
James Ko <jamesqko@gmail.com>
2018-02-20 17:34:15 -0500

    Properly tokenize id/description in SmartTagger

--- SmartTagger.py
+++ SmartTagger.py
@@ -1,10 +1,15 @@
 import enchant
 import logging as log
 
+from sklearn.feature_extraction.text import CountVectorizer
+
 class SmartTagger(object):
     def __init__(self, weights={'description': 3, 'id': 5, 'tags': 1}):
         self._english = enchant.Dict('en_US')
         self.weights = weights
+    
+    def _is_hack_word(self, term):
+        return not self._english.check(term)
     
     def _enrich_tags(self, row):
         tags = row['tags']
@@ -13,15 +18,11 @@ class SmartTagger(object):
         else:
             tag_weights = {}
 
-        for term in row['description'].split():
-            term = term.lower()
-            if term in self.tags_vocab_ and not self._english.check(term):
-                tag_weights[term] = tag_weights.get(term, 0) + self.weights['description']
-
-        for term in row['id'].split('.'):
-            term = term.lower()
-            if term in self.tags_vocab_ and not self._english.check(term):
-                tag_weights[term] = tag_weights.get(term, 0) + self.weights['id']
+        for feature in 'description', 'id':
+            tag_counts = self._cv.transform([row[feature]])
+            for index in tag_counts.nonzero()[1]:
+                term = self._cv_vocab[index]
+                tag_weights[term] = tag_weights.get(term, 0) + self.weights[feature]
 
         etags = [f'{pair[0]} {pair[1]}' for pair in sorted(tag_weights.items())]
         rowcopy = row.copy()
@@ -32,5 +33,8 @@ class SmartTagger(object):
         return rowcopy
 
     def fit_transform(self, df):
-        self.tags_vocab_ = set([tag.lower() for tags in df['tags'] for tag in tags.split(',')])
+        self.tags_vocab_ = set([tag.lower() for tags in df['tags'] for tag in tags.split(',') if tag])
+        self._cv_vocab = list(filter(self._is_hack_word, self.tags_vocab_))
+        self._cv = CountVectorizer(vocabulary=self._cv_vocab)
+
         return df.apply(self._enrich_tags, axis=1)
