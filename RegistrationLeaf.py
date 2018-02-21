81dfd00ae1631a7f8f66ace360002e75e7a65100
James Ko <jamesqko@gmail.com>
2018-02-21 01:35:09 -0500

    Handle remote pages in RegistationPage

--- RegistrationLeaf.py
+++ RegistrationLeaf.py
@@ -1,14 +1,12 @@
 class RegistrationLeaf(object):
     def __init__(self, json):
-        node = json['catalogEntry']
-
-        self.authors = node.get('authors', [])
-        self.description = node.get('description', "")
-        self.icon_url = node.get('iconUrl', '')
-        self.id = node['id']
-        self.license_url = node.get('licenseUrl', '')
-        self.listed = node.get('listed', True)
-        self.project_url = node.get('project_url', '')
-        self.summary = node.get('summary', "")
-        self.tags = node.get('tags', [])
-        self.version = node['version']
+        self.authors = json.get('authors', [])
+        self.description = json.get('description', "")
+        self.icon_url = json.get('iconUrl', '')
+        self.id = json['id']
+        self.license_url = json.get('licenseUrl', '')
+        self.listed = json.get('listed', True)
+        self.project_url = json.get('project_url', '')
+        self.summary = json.get('summary', "")
+        self.tags = json.get('tags', [])
+        self.version = json['version']
--- RegistrationPage.py
+++ RegistrationPage.py
@@ -1,9 +1,18 @@
 from RegistrationLeaf import RegistrationLeaf
 
 class RegistrationPage(object):
-    def __init__(self, json):
+    def __init__(self, json, load=True):
         self.count = json['count']
-        self._leaves = [RegistrationLeaf(json=node) for node in json['items']]
+        self._json = json
+        if load:
+            self.load()
     
     def __iter__(self):
         return iter(self._leaves)
+
+    def load(self):
+        if not 'items' in self._json:
+            url = self._json['@id']
+            self._json = get_as_json(url)
+        self._leaves = [RegistrationLeaf(json=node['catalogEntry']) for node in self._json['items']]
+        return self
