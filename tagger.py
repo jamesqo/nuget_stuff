9a09c8fba42e336c85d8bb367e780ff83bfb3edd
James Ko <jamesqko@gmail.com>
2018-04-20 21:30:45 -0400

    Workaround for NuGet bug: https://github.com/NuGet/NuGetGallery/issues/5836

--- tagger.py
+++ tagger.py
@@ -22,7 +22,11 @@ def _is_hackword(term):
     return not ENGLISH.check(term)
 
 def _parse_tags(text):
-    return [tag.lower() for tag in text.split(',') if tag]
+    # Why `tag2` is necessary: see https://github.com/NuGet/NuGetGallery/issues/5836
+    # Some tag values mistakenly have newlines in them, e.g. 1 tag named 'foo\r\nbar'
+    # when the user actually meant to create 2 tags named 'foo' and 'bar'.
+    return [tag2.strip() for tag in text.split(',') if tag.strip()
+            for tag2 in tag.split() if tag2.strip()]
 
 def _compute_idfs(df):
     log_call()
