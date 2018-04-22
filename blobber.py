0f4a7bfc567742b54c98127a88e7ba303596c13f
James Ko <jamesqko@gmail.com>
2018-04-21 23:30:45 -0400

    Split pages if OOM is raised while training

--- blobber.py
+++ blobber.py
@@ -1,3 +1,4 @@
+import gc
 import logging
 import os
 import shutil
@@ -25,32 +26,52 @@ def pagenos(df):
     assert all(~df['pageno'].isna())
     return sorted(set(df['pageno']))
 
+def predict_for_part(df, feats, parentdf, chunkmgr):
+    try:
+        M, m = parentdf.shape[0], df.shape[0] # Good
+        magic = Recommender(n_recs=5, # TODO: This should be a command-line option
+                            mode='chunked',
+                            n_total=M,
+                            n_pred=m)
+
+        for chunkno in chunknos(parentdf):
+            df_fit, feats_fit = get_chunk(parentdf, chunkno), chunkmgr.load(chunkno)
+
+            magic.partial_fit(X=feats_fit,
+                              df=df_fit,
+                              X_pred=feats,
+                              df_pred=df)
+
+        return magic.predict(feats, df)
+    except MemoryError:
+        LOG.debug("Encountered MemoryError, splitting DataFrame into 2 parts")
+        if m < 2:
+            raise RuntimeError("Can't split DataFrame anymore")
+        split = m // 2
+
+        df1, feats1 = df.loc[:split], feats[:split, :]
+        df2, feats2 = df.loc[split:], feats[split:, :]
+
+        # TODO: Ensure the gc is picking up on the fact that we're no longer using df/feats.
+        gc.collect()
+
+        recs1 = predict_for_part(df1, feats1, parentdf, chunkmgr)
+        recs2 = predict_for_part(df2, feats2, parentdf, chunkmgr)
+        recs1.update(recs2)
+        return recs1
+
 def gen_blobs_for_page(pageno, df, feats, parentdf, blobs_root, chunkmgr):
     dirname = os.path.join(blobs_root, 'page{}'.format(pageno))
     LOG.debug("Generating blobs for page #{} in {}", pageno, dirname)
 
-    M, m = parentdf.shape[0], df.shape[0] # Good
-    magic = Recommender(n_recs=5, # TODO: This should be a command-line option
-                        mode='chunked',
-                        n_total=M,
-                        n_pred=m)
-
-    for chunkno in chunknos(parentdf):
-        df_fit, feats_fit = get_chunk(parentdf, chunkno), chunkmgr.load(chunkno)
-
-        magic.partial_fit(X=feats_fit,
-                          df=df_fit,
-                          X_pred=feats,
-                          df_pred=df)
-
-    recs_dict = magic.predict(feats, df)
+    recs_dict = predict_for_part(df, feats, parentdf, chunkmgr)
 
     os.makedirs(dirname, exist_ok=True)
     ids = list(df['id'])
     for id_ in ids:
         hexid = id_.encode('utf-8').hex()
         blob_fname = os.path.join(dirname, '{}.json'.format(hexid))
-        if is_windows:
+        if is_windows: # pylint: disable=W0125
             blob_fname = extended_path(blob_fname)
 
         recs = recs_dict[id_]
